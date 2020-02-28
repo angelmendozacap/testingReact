57f9249e7619a6e31ca300b539a16f1f1b28f69e
Angel Mendoza <1930.kick@gmail.com>
2020-02-27 23:38:56 -0500

    Implement React Redux

--- src/Components/Organisms/CourseGrid.jsx
+++ src/Components/Organisms/CourseGrid.jsx
@@ -6,7 +6,16 @@ export default class CourseGrid extends Component {
     return (
       <section className="py-8 grid gap-8 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
         {
-          this.props.courses.map(course => <CourseCard image={course.image} title={course.title} description={course.description} id={course.id} key={course.id} />)
+          this.props.courses.map(course => (
+            <CourseCard
+              price={course.price}
+              image={course.image}
+              title={course.title}
+              description={course.description}
+              id={course.id}
+              key={course.id}
+            />
+          ))
         }
       </section>
     )
---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import { createStore } from "redux"
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./action"
+
+const initialStore = {
+  cart: []
+}
+
+const rootReducer = (state = initialStore, { type, id }) => {
+  
+  if (type === ADD_TO_CART) {
+
+    if (state.cart.find(a => a === id)) return state
+
+    return {
+      ...state,
+      cart: state.cart.concat(id)
+    }
+  }
+
+  if (type === REMOVE_FROM_CART) {
+    return {
+      ...state,
+      cart: state.cart.filter(a => a !== id)
+    }
+  }
+
+  return state
+}
+
+export default createStore(rootReducer)
